657045601ca65d8f563c8c1813d3a086baf64249
Liam O'Donnell <spikything@gmail.com>
2022-05-19 23:54:31 +0100

    Add list of existing todos to the UI

---
+++ client/src/components/ListTodos.js
@@ -0,0 +1,62 @@
+import React, { Fragment, useEffect, useState } from 'react';
+
+const ListTodos = () => {
+
+    const [todos, setTodos] = useState([]);
+
+    const deleteTodo = async id => {
+        try {
+            const deleteTodo = await fetch(`http://localhost:5000/todos/${id}`, {
+                method: "DELETE"
+            });
+            
+            setTodos(todos.filter(todo => todo.todo_id !== id));
+        } catch (error) {
+            console.error(error.message);
+        }
+    };
+
+    const getTodos = async () => {
+        try {
+            const response = await fetch("http://localhost:5000/todos");
+            const jsonData = await response.json();
+            setTodos(jsonData);
+        } catch (error) {
+            console.error(error.message);
+        }
+    }
+
+    useEffect(() => {
+        getTodos();
+    }, []);
+
+    return <Fragment>
+        <table className="table mt-5 text-center">
+            <thead>
+            <tr>
+                <th>Description</th>
+                <th>Edit</th>
+                <th>Delete</th>
+            </tr>
+            </thead>
+            <tbody>
+            
+            {todos.map(todo => (
+                <tr key={todo.todo_id}>
+                    <td>{todo.description}</td>
+                    <td>
+                        <button 
+                            className='btn btn-danger'
+                            onClick={() => deleteTodo(todo.todo_id)}
+                        >Delete</button>
+                    </td>
+                </tr>
+            ))}
+
+            </tbody>
+        </table>
+    </Fragment>
+    
+};
+
+export default ListTodos;
