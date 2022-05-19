7d9c8b4e7a4e2f41187ac18f5d4265d7b3505eee
Liam O'Donnell <spikything@gmail.com>
2022-05-19 23:26:26 +0100

    Adding skeletal client with create-react-app

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # perntest
 A demo app based on the PERN stack (PostgreSQL, Express, React, Node)
+
+# server
+Run the server with
+
+```
+node index
+```
+
+# client
+coming soon
--- server/index.js
+++ server/index.js
@@ -8,6 +8,18 @@ const PORT = 5000;
 app.use(cors());
 app.use(express.json());
 
+// add a todo
+app.post('/todos', async (req, res) => {
+    try {
+        const {description} = req.body;
+        const newTodo = await pool.query("INSERT INTO todo (description) VALUES($1) RETURNING *", [description]);
+        res.json(newTodo.rows[0]);
+    } catch (error) {
+        console.log(error.message);
+    }
+});
+
+// start the server
 app.listen(PORT, () => {
     console.log("server started on port " + PORT);
 });
